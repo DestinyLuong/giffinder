09bfa79a727c26a744ba3eac1842541fe8f6cc42
Destiny Luong <destinyul2010@gmail.com>
2018-03-01 01:16:12 +0000

    Finished all Functions & Math.Random

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+  function giphyURLWithSearchTerm(searchTerm) {
+      // write a function that will return a url for the giphy API with
+      // the searchTerm provided in the parameters
+      var url = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+      return url;
+  }
+
+  function appendImageToGallery(srcURL) {
+      // write a function that will append an <img> to the div with class="gallery"
+      // using the URL provided in the parameters
+      for(var i = 0; i > 10; i ++){
+       $(".gallery").append("<img src=" + srcURL + ">");
+      }
+  }
+
+  function callGiphyAPIWithSearchTerm(searchTerm) {
+      // use $.ajax to call the giphy api with the given search term from the parameters.
+      // get the first image url from the ajax response
+      // use appendImageToGallery to put the image onto the screen
+      var giphyURL = giphyURLWithSearchTerm(searchTerm);
+      $.ajax({
+      url: giphyURL,
+      method: "GET",
+      success: function(response) {
+       var random = Math.floor(Math.random() * giphyURL.length);
+       var url = response.data[random].images.original.url;
+       appendImageToGallery(url);
+       
+      },
+    }); 
+
+  }
+
 
 $(document).ready(function(){
   
@@ -10,39 +43,8 @@ $(document).ready(function(){
     2. get the typed input from the search input box
     3. call the functions below!
   */
-  
-  function giphyURLWithSearchTerm(searchTerm) {
-      // write a function that will return a url for the giphy API with
-      // the searchTerm provided in the parameters
-      var url = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
-  }
-
-  function appendImageToGallery(srcURL) {
-      // write a function that will append an <img> to the div with class="gallery"
-      // using the URL provided in the parameters
-       $("body").append("<img src=" + srcURL + ">");
-  }
-
-  function callGiphyAPIWithSearchTerm(searchTerm) {
-      // use $.ajax to call the giphy api with the given search term from the parameters.
-      // get the first image url from the ajax response
-      // use appendImageToGallery to put the image onto the screen
-      $.ajax({
-      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
-      method: "GET",
-      success: function(response) {
-           // Log the whole response to the console
-            //console.log(response);
-            
-           // Log the first image of the data to the console
-            //var one = response["data"];
-            //console.log(one[0].url);
-           // Log the "type" property of1 the first image object to the console
-            //console.log(one[0].type);
-           // Log the "title" property of the first image object to the console
-            //console.log(one[0].title);
-      },
-    }); 
-
-  }
+  $("#btn").click(function(){
+   var type = $("#srch-term").val();
+   callGiphyAPIWithSearchTerm(type);
+  });
 });
